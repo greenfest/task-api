2edc71ad8fb6632a1df756f05973ca3d0752ec00
Oleg Razin <1greenfest@gmail.com>
2023-09-08 00:02:58 +0500

    added routes to tasks.js

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const tasks = require('./routes/tasks');
-
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/tasks", tasks);
-
-app.use((err, _req, res) => {
-    res.status(500).send("Uh oh! An unexpected error occured.")
-})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
